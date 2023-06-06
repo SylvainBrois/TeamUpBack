18a7d835304d99082c7ee4a0b9f4a3f0178239e4
sylvainB <brois8572@gmail.com>
2023-06-06 10:12:06 +0200

    Init

---
+++ src/main/kotlin/com/example/teamupback/TeamUpBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.teamupback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamUpBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamUpBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/teamupback/TeamUpBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.teamupback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamUpBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
